4918d8a63704030c41aca20eed24f4ddb0636e76
Aeforeva <83841383+Aeforeva@users.noreply.github.com>
2023-01-03 19:01:34 +0300

    item layout

--- app/src/main/java/com/example/vatcalculator/adapters/BindingAdapters.kt
+++ app/src/main/java/com/example/vatcalculator/adapters/BindingAdapters.kt
@@ -2,11 +2,22 @@ package com.example.vatcalculator.adapters
 
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.example.vatcalculator.R
 
 @BindingAdapter("buttonVisibility")
 fun setVisibility(button: Button, show: Boolean) {
     if (show) button.visibility = View.VISIBLE else button.visibility = View.GONE
+}
+
+@BindingAdapter("lockStatus")
+fun setLockImage(imageView: ImageView, isLocked: Boolean) {
+    if (isLocked) {
+        imageView.setImageResource(R.drawable.lock_close)
+    } else {
+        imageView.setImageResource(R.drawable.lock_open)
+    }
 }
 
 // Not yet used (For now i just passed true/false from viewModel directly to isEnabled in layout)
